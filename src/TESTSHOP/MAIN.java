b04d809b16eeff0003ba0a956c58049257e4b20e
mbush1 <bushkov.mikhail@gmail.com>
2023-12-16 18:33:32 -0500

    tester

--- src/TESTSHOP/MAIN.java
+++ src/TESTSHOP/MAIN.java
@@ -3,11 +3,14 @@ package TESTSHOP;
 import javax.swing.*;
 import static TESTSHOP.WINDOWMANAGER.createWindow;
 
+// sigma ohio
+
 public class MAIN
 {
     public static void main(String[] args)
     {
         JFrame frame = createWindow();
+        System.out.println("Started the program; window opened.");
         frame.setVisible(true);
 
         Thread thread = new Thread(new WINDOWMANAGER());
