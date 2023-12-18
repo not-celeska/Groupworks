d9a0b6995de89ddeb27d52d42977aa2fac0f1355
not-celeska <bushkov.mikhail@gmail.com>
2023-12-17 19:38:49 -0500

    Dev post 2 - game gui started

--- src/furnishsim/GuiManager.java
+++ src/furnishsim/GuiManager.java
@@ -2,9 +2,10 @@ package furnishsim;
 
 import javax.swing.*;
 
-public class GuiManager extends JFrame
+public class GuiManager
 {
     Business gameState;
+    JFrame gameWindow;
 
     public GuiManager(Business gameState)
     {
@@ -13,6 +14,20 @@ public class GuiManager extends JFrame
 
     public void runSimulation()
     {
+        gameWindow = new JFrame();
+        JPanel infoScreen = new JPanel();
+        JLabel businessData = new JLabel(gameState.toString());
+        JButton tickButton = new JButton("TICK");
+        infoScreen.add(businessData);
+        infoScreen.add(tickButton);
+        gameWindow.add(infoScreen);
+
         System.out.println("FurnishSim has started!!!");
+        gameWindow.setSize(700, 150);
+        gameWindow.setResizable(true);
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setVisible(true);
+
     }
 }
